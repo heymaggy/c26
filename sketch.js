8263c1f4e190e986de9dc83dfc54e6ddf3085004
Delicia Magdaline <deliciamagdaline@Delicias-MacBook-Pro.local>
2020-11-25 17:39:11 +0530

    added a box

--- sketch.js
+++ sketch.js
@@ -19,6 +19,7 @@ function setup(){
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
+    box6 = new Box(100,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
@@ -53,6 +54,7 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
+    box6.display();
 
     box5.display();
     log4.display();
